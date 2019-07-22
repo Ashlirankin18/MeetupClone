a6261b77c8ad85b70379b830b968a9e3ad01322f
Ashli Rankin <ashlirankin18@gmail.com>
2019-07-22 15:54:56 -0400

    Created MeetupGroupModel, MeetupUserModel and UserRSVPModel

---
+++ MeetupClone/Models/MeetupUserModel.swift
@@ -0,0 +1,42 @@
+//
+//  MeetupUserModel.swift
+//  MeetupClone
+//
+//  Created by Ashli Rankin on 7/22/19.
+//  Copyright © 2019 Lickability. All rights reserved.
+//
+
+import Foundation
+
+struct MeetupUserModel: Codable{
+    let bio: String?
+    let city: String?
+    let country: String?
+    let id: Int?
+    let joinedDate: Int
+    let userName: String
+    let photo: Photo?
+    
+    private enum CodingKeys: String, CodingKey {
+        case bio = "bio"
+        case city = "city"
+        case country = "country"
+        case id = "id"
+        case joinedDate = "joined"
+        case userName = "name"
+        case photo = "photo"
+        
+    }
+}
+struct Photo: Codable {
+    let baseURL: URL?
+    let highresLink: URL?
+    let thumbnailLink: URL?
+    
+    private enum CodingKeys: String, CodingKey {
+        case baseURL = "base_url"
+        case highresLink = "highres_link"
+        case thumbnailLink = "thumb_link"
+    }
+}
+
---
+++ MeetupClone/Models/UserRSVPModel.swift
@@ -0,0 +1,33 @@
+//
+//  UserRSVPModel.swift
+//  MeetupClone
+//
+//  Created by Ashli Rankin on 7/22/19.
+//  Copyright © 2019 Lickability. All rights reserved.
+//
+
+import Foundation
+
+struct UserRSVPModel: Codable {
+    let createdDate: Int
+    let member: Member
+    let response: String
+    
+    private enum CodingKeys: String, CodingKey {
+        case createdDate = "created"
+        case member = "member"
+        case response = "response"
+    }
+}
+struct Member:Codable {
+    let id: Int
+    let userName:String
+    let photo: Photo
+    
+    private enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case userName = "name"
+        case photo = "photo"
+    }
+}
+
