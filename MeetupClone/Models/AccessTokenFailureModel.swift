da8e6020b52a813f0e2eb76725865983ce898f57
Ashli Rankin <ashlirankin18@gmail.com>
2019-07-22 15:26:40 -0400

    Created the Access token success and failure models, Cleaned up the boilerplate code in the App Delegate.

---
+++ MeetupClone/Models/AccessTokenFailureModel.swift
@@ -0,0 +1,13 @@
+//
+//  AccessTokenFailureModel.swift
+//  MeetupClone
+//
+//  Created by Ashli Rankin on 7/22/19.
+//  Copyright © 2019 Lickability. All rights reserved.
+//
+
+import Foundation
+
+struct AccessTokenFailureModel: Codable {
+    let error: String
+}
