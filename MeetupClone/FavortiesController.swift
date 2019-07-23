25ecde491b4983ec4f12d2c17e2913aa37200765
Ashli Rankin <ashlirankin18@gmail.com>
2019-07-23 09:59:28 -0400

    Added View Controllers to the meetupInterfaceController.storyboard Added View Controller files

---
+++ MeetupClone/FavortiesController.swift
@@ -0,0 +1,19 @@
+//
+//  FavortiesController.swift
+//  MeetupClone
+//
+//  Created by Ashli Rankin on 7/23/19.
+//  Copyright © 2019 Lickability. All rights reserved.
+//
+
+import UIKit
+
+class FavortiesController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+
+}
