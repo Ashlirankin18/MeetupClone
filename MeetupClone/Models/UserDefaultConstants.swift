495c39b846d69869264d6ab8cb8e7681671038f2
Ashli Rankin <ashlirankin18@gmail.com>
2019-07-23 12:05:38 -0400

    Added a constants file for userDefaults.

---
+++ MeetupClone/Models/UserDefaultConstants.swift
@@ -0,0 +1,14 @@
+//
+//  UserDefaultConstants.swift
+//  MeetupClone
+//
+//  Created by Ashli Rankin on 7/23/19.
+//  Copyright © 2019 Lickability. All rights reserved.
+//
+
+import Foundation
+
+struct UserDefaultConstants {
+     static let lodaingToken = "loadingOAuthToken"
+     static let accessToke = "accessToken"
+}
