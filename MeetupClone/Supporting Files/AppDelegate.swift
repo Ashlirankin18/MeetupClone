99813d969fe0c070d0a419a1f4083accbb708f7e
Ashli Rankin <ashlirankin18@gmail.com>
2019-07-25 09:23:50 -0400

    added viewController to interface storboard. Added a textView to hold the user bio information

--- MeetupClone/Supporting Files/AppDelegate.swift
+++ MeetupClone/Supporting Files/AppDelegate.swift
@@ -12,29 +12,13 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
-
+    let apiManager = MeetupAuthenticationHandler(userDefaults: UserDefaults.standard, networkHelper: NetworkHelper())
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-<<<<<<< HEAD
-        if !apiManager.hasOAuthToken() {
-            guard let loginViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "LoginViewController") as? LoginViewController else {
-                return false }
-            loginViewController.apiManager = apiManager
-            window?.rootViewController = loginViewController
-            window?.makeKeyAndVisible()
-        } else {
-            guard let meetupUserInterface = UIStoryboard(name: "MeetupInfoInterface", bundle: nil).instantiateViewController(withIdentifier: "MeetupInfoTabbarController") as? UITabBarController else {
-                return false }
-            window?.rootViewController = meetupUserInterface
-            window?.makeKeyAndVisible()
-        }
         return true
     }
     
     func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
         apiManager.processAuthorizationResponse(url: url)
-=======
-       
->>>>>>> parent of 3c3034e... Added code to the a--delegate which check for a OAuthToken. Based on this result the root viewController of the window is set
         return true
     }
 }
