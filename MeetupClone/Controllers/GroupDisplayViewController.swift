fc0e510c1acea492ee62e07cf5cc8ba0ee54598c
Ashli Rankin <ashlirankin18@gmail.com>
2019-07-26 14:35:40 -0400

    Added a tableView to the groupDisplayViewController. Connected it to the groupDisplayController file. Fixed minor spelling issue in LoginViewController

--- MeetupClone/Controllers/GroupDisplayViewController.swift
+++ MeetupClone/Controllers/GroupDisplayViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 /// Display a list of groups that the user searches for.
 class GroupDisplayViewController: UIViewController {
 
+    @IBOutlet weak var groupDisplayTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
