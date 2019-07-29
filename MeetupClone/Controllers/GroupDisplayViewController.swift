bfe30692270e986273045e12d4bbb43bdb75b9bd
Ashli Rankin <ashlirankin18@gmail.com>
2019-07-29 12:11:01 -0400

    Added network retrieval code to  GroupDisplayViewController

--- MeetupClone/Controllers/GroupDisplayViewController.swift
+++ MeetupClone/Controllers/GroupDisplayViewController.swift
@@ -10,8 +10,22 @@ import UIKit
 
 /// Display a list of groups that the user searches for.
 class GroupDisplayViewController: UIViewController {
-
+    
+    private let meetUpDataHandler = MeetupDataHandler(networkHelper: NetworkHelper())
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        retrieveGroups()
+    }
+    
+    private func retrieveGroups(){
+        meetUpDataHandler.retrieveMeetupGroups(zipCode: 11429) { (result) in
+            switch result {
+            case .failure(let error):
+                print(error.localizedDescription)
+            case .success(let groups):
+                print(groups)
+            }
+        }
     }
 }
