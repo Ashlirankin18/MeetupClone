6b4d810e82d4c0a12c0510f4153bbcd856a3d806
Ashli Rankin <ashlirankin18@gmail.com>
2019-07-29 12:26:10 -0400

    Refactored the 	MeetupDataHandler added a function to retrieve groups from the server

--- MeetupClone/Networking/MeetupDataHandler.swift
+++ MeetupClone/Networking/MeetupDataHandler.swift
@@ -13,22 +13,23 @@ import Foundation
 class MeetupDataHandler {
     
     private var networkHelper: NetworkHelper
-    
+    var meetupAuthenticatiorHandler = MeetupAuthenticationHandler(userDefaults: UserDefaults.standard, networkHelper: NetworkHelper())
     /// Initializes the class with networkHelper.
     init(networkHelper: NetworkHelper) {
         self.networkHelper = networkHelper
     }
     
-    typealias Handler = (Result<MeetupUserModel, AppError>) -> Void
+    typealias UserHandler = (Result<MeetupUserModel, AppError>) -> Void
+    typealias GroupHandler = (Result<[MeetupGroupModel], AppError>) -> Void
     
     /// Retrieves the user data from the server.
     /// - Parameter accessToken: Access token that was returned from the server.
     /// - Parameter completionHandler: receives information (expected type) when asynchronous call completes.
-    func retrieveUserData(accessToken: String, completionHandler: @escaping Handler) {
-        
+    func retrieveUserData(completionHandler: @escaping UserHandler) {
+        let accessToken = meetupAuthenticatiorHandler.accessToken
+        let bearer = ("Bearer \(accessToken)", "Authorization")
         let urlString = "https://api.meetup.com/2/member/self"
-        
-        networkHelper.performDataTask(URLEndpoint: urlString, httpMethod: .Post, httpBody: nil, httpHeader: ("Bearer \(accessToken)", "Authorization")) { (results) in
+        networkHelper.performDataTask(URLEndpoint: urlString, httpMethod: .Post, httpBody: nil, httpHeader: bearer) { (results) in
             switch results {
             case .failure(let error):
                 completionHandler(.failure(.networkError(error)))
@@ -41,6 +42,27 @@ class MeetupDataHandler {
                 } catch {
                     completionHandler(.failure(.decodingError("Could not decode UserModel from data")))
                     return
+                }
+            }
+        }
+    }
+    
+    func retrieveMeetupGroups(zipCode: Int?, completionHandler: @escaping GroupHandler) {
+        let accessToken = UserDefaults.standard.object(forKey: UserDefaultConstants.accessToken.rawValue) ?? ""
+        var urlString = ""
+        urlString = zipCode == nil ? "https://api.meetup.com/find/groups?&sign=true&photo-host=public&page=20" : "https://api.meetup.com/find/groups?&sign=true&photo-host=public&zip=\(11429)&page=20"
+        networkHelper.performDataTask(URLEndpoint: urlString, httpMethod: .Get, httpBody: nil, httpHeader: ("Bearer \(accessToken)", "Authorization")) { (results) in
+            switch results {
+            case .failure(let error):
+                completionHandler(.failure(.networkError(error)))
+                return
+            case .success(let data):
+                do {
+                   let groups = try JSONDecoder().decode([MeetupGroupModel].self, from: data)
+                 completionHandler(.success(groups))
+                    return
+                } catch {
+                    print(error.localizedDescription)
                 }
             }
         }
