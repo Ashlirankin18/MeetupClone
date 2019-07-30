50dabe3881af799807b326560d1636470ebe1ced
Ashli Rankin <ashlirankin18@gmail.com>
2019-07-30 15:16:36 -0400

    Added a bar button to the storyboard file. also changed the navbar to prefers large titles

--- MeetupClone/Controllers/GroupDisplayViewController.swift
+++ MeetupClone/Controllers/GroupDisplayViewController.swift
@@ -10,8 +10,34 @@ import UIKit
 
 /// Display a list of groups that the user searches for.
 class GroupDisplayViewController: UIViewController {
-
+    
+    @IBOutlet private weak var zipCodeButton: UIBarButtonItem!
+    
+    private lazy var searchController: UISearchController = {
+        let searchController = UISearchController(searchResultsController: nil)
+        searchController.definesPresentationContext = true
+        return searchController
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setNavigationSpecifices()
+        setupSearchController()
+    }
+    
+    private func setupSearchController() {
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.placeholder = "Search for Groups"
+    }
+    private func setNavigationSpecifices() {
+        navigationItem.searchController = searchController
+        navigationController?.navigationBar.prefersLargeTitles = true
+        definesPresentationContext = true
+    }
+    @IBAction private func zipCodeButtonPressed(_ sender: UIBarButtonItem) {
+    }
+}
+extension GroupDisplayViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
     }
 }
