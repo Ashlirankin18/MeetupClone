5b2f6d332d514dfab71ecc9251bd343b65adf886
Ashli Rankin <ashlirankin18@gmail.com>
2019-08-01 13:16:46 -0400

    Created EventTableViewControllerDaraSource

---
+++ MeetupClone/Controllers/DataSource/EventsDisplayTableViewControllerDataSource.swift
@@ -0,0 +1,22 @@
+//
+//  EventsDisplayTableViewControllerDataSource.swift
+//  MeetupClone
+//
+//  Created by Ashli Rankin on 8/1/19.
+//  Copyright © 2019 Lickability. All rights reserved.
+//
+
+import UIKit
+
+class EventsDisplayTableViewControllerDataSource: NSObject, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        return cell
+    }
+    
+
+}
