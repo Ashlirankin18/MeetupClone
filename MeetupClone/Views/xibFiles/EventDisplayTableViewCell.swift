ad3fdaaa1a300985dd2f5f5d28ef53889393a120
Ashli Rankin <ashlirankin18@gmail.com>
2019-08-01 13:19:56 -0400

    Created EventsDisplayTableViewCell to display event details

---
+++ MeetupClone/Views/xibFiles/EventDisplayTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  EventDisplayTableViewCell.swift
+//  MeetupClone
+//
+//  Created by Ashli Rankin on 8/1/19.
+//  Copyright © 2019 Lickability. All rights reserved.
+//
+
+import UIKit
+
+class EventDisplayTableViewCell: UITableViewCell {
+    @IBOutlet private weak var eventNameLabel: UILabel!
+    @IBOutlet private weak var eventDescriptionTextView: UITextView!
+    @IBOutlet private weak var eventLocationLabel: UILabel!
+    @IBOutlet private weak var rsvpLabel: UILabel!
+    
+}
