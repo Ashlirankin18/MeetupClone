23f94ab63ae76158579c5bea34a2b695895b6346
Ashli Rankin <ashlirankin18@gmail.com>
2019-08-07 15:10:18 -0400

    Refactored the UI of the event display cell

--- MeetupClone/Views/xibFiles/EventDisplayTableViewCell.swift
+++ MeetupClone/Views/xibFiles/EventDisplayTableViewCell.swift
@@ -12,5 +12,24 @@ class EventDisplayTableViewCell: UITableViewCell {
     @IBOutlet private weak var eventNameLabel: UILabel!
     @IBOutlet private weak var eventDescriptionTextView: UITextView!
     @IBOutlet private weak var eventLocationLabel: UILabel!
-    @IBOutlet private weak var rsvpLabel: UILabel!    
+    @IBOutlet private weak var rsvpLabel: UILabel!
+    
+    struct ViewModel {
+        let eventName: String
+        let eventDescription: String
+        let eventLocation: String
+        let rsvpCount: Int
+    }
+    
+    var viewModel: ViewModel? {
+        didSet {
+            guard let viewModel = viewModel else {
+                return
+            }
+            eventNameLabel.text = viewModel.eventName
+            eventDescriptionTextView.text = viewModel.eventDescription
+            eventLocationLabel.text = viewModel.eventLocation
+            rsvpLabel.text = "\(viewModel.rsvpCount)"
+        }
+    }    
 }
