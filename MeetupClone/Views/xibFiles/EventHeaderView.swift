b99b9b4bc17379f69d339c578297fddce05d3e54
Ashli Rankin <ashlirankin18@gmail.com>
2019-08-08 15:32:18 -0400

    Created EventHeaderView this will display the header containing a mapView of where the event is located.

---
+++ MeetupClone/Views/xibFiles/EventHeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  EventHeaderView.swift
+//  MeetupClone
+//
+//  Created by Ashli Rankin on 8/8/19.
+//  Copyright © 2019 Lickability. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class EventHeaderView: UIView {
+    @IBOutlet private weak var eventLocationMapView: MKMapView!
+    
+    @IBOutlet private weak var eventNameLabel: UILabel!
+    
+    @IBOutlet private weak var eventLocationLabel: UILabel!
+}
