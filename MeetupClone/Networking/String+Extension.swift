f8f08a37f45bd37ada363433b123293314d4cc4c
Ashli Rankin <ashlirankin18@gmail.com>
2019-08-09 16:01:44 -0400

    Added documentation to the String Ext

--- MeetupClone/Networking/String+Extension.swift
+++ MeetupClone/Networking/String+Extension.swift
@@ -7,7 +7,13 @@
 
 import Foundation
 
+// MARK: - Extension to the String class
 extension String {
+    
+    /// Will convert a String with possible HTML syntax to a human readable format
+    ///
+    /// - Returns: An string which will display in an human readable format.
+    /// - Throws: This function will throw an error if an attribute string could not be created
     func convertHTMLStrings() throws -> NSAttributedString {
         if let data = self.data(using: .utf8),
             let attributedString = try? NSAttributedString(data: data, options: [.documentType: NSAttributedString.DocumentType.html], documentAttributes: nil) {
