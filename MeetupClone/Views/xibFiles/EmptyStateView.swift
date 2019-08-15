bb9aa79ef2e2df41203f91f399f566e31e7d1ae3
Ashli Rankin <ashlirankin18@gmail.com>
2019-08-15 09:41:05 -0400

    Added an view model to the `EmptyStateView`

--- MeetupClone/Views/xibFiles/EmptyStateView.swift
+++ MeetupClone/Views/xibFiles/EmptyStateView.swift
@@ -8,8 +8,32 @@
 
 import UIKit
 
-class EmptyStateView: UIView {
+/// 'UIView' subclass which will display a prompt to the user if the object array is empty.
+final class EmptyStateView: UIView {
 
-   
-
+    /// Manages the data that is needed to populate the `EmptyStateView`
+    struct ViewModel {
+        
+        /// The display image
+        let image: UIImage
+        
+        /// The prompt shown when there are no results
+        let prompt: String
+    }
+    
+    @IBOutlet private weak var emptyStateImage: UIImageView!
+    
+    @IBOutlet private weak var emptyStatePrompt: UILabel!
+    
+    /// Represents the `EmptyStateView`'s model
+    var viewModel: ViewModel? {
+        didSet {
+            guard let viewModel = viewModel else {
+                assertionFailure("No viewModel was found")
+                return
+            }
+            emptyStateImage.image = viewModel.image
+            emptyStatePrompt.text = viewModel.prompt
+        }
+    }
 }
