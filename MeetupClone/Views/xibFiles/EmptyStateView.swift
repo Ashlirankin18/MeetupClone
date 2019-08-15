fc0bc5f501e4833758ca40ef6d6b0add47038461
Ashli Rankin <ashlirankin18@gmail.com>
2019-08-15 09:21:58 -0400

    Deleted the `emptySateViewController` added an EmptyStateView

---
+++ MeetupClone/Views/xibFiles/EmptyStateView.swift
@@ -0,0 +1,15 @@
+//
+//  EmptyStateView.swift
+//  MeetupClone
+//
+//  Created by Ashli Rankin on 8/15/19.
+//  Copyright © 2019 Lickability. All rights reserved.
+//
+
+import UIKit
+
+class EmptyStateView: UIView {
+
+   
+
+}
