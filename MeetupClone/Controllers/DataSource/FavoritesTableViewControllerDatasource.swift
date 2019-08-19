d9ff2bcbcaf1c82a9931ec099f535be4525b019f
Ashli Rankin <ashlirankin18@gmail.com>
2019-08-19 15:21:13 -0400

    Update FavoritesTableViewControllerDatasource.swift

--- MeetupClone/Controllers/DataSource/FavoritesTableViewControllerDatasource.swift
+++ MeetupClone/Controllers/DataSource/FavoritesTableViewControllerDatasource.swift
@@ -9,21 +9,21 @@
 import UIKit
 
 ///  DataSource which will be used to mage data for the FavoritesTableViewController.
-final class FavoritesTableViewControllerDatasource: NSObject, UITableViewDataSource {
+final class FavoritesTableViewControllerDataSource: NSObject, UITableViewDataSource {
     
     // MARK: - UITableViewDataSource
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return PersistenceHelper.retrieveFavoriteEventsFromDocumentsDirectory().count
+        return PersistenceHelper.shared.retrieveFavoriteEventsFromDocumentsDirectory().count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "EventDisplayCell", for: indexPath) as? EventDisplayTableViewCell else {
             return UITableViewCell()
         }
-        let event = PersistenceHelper.retrieveFavoriteEventsFromDocumentsDirectory().reversed()[indexPath.row]
+        let event = PersistenceHelper.shared.retrieveFavoriteEventsFromDocumentsDirectory().reversed()[indexPath.row]
         
-        cell.viewModel = EventDisplayTableViewCell.ViewModel(eventName: event.eventName, eventDescription: event.description ?? NSLocalizedString("This event does not have a description", comment: "Informs the user the event has no description currently."), eventLocation: event.eventCity, rsvpCount: event.rsvpCount)
+        cell.viewModel = EventDisplayTableViewCell.ViewModel(eventName: event.eventName, eventDescription: event.description ?? NSLocalizedString("This event does not have a description", comment: "Informs the user the event has no description currently."), eventLocation: event.venue?.city, rsvpCount: event.yesRSVPCount)
         return cell
     }
 }
