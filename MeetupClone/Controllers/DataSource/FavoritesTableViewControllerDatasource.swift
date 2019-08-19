46e17d3873d0c380ddab6d94e0f73870d236cf42
Ashli Rankin <42153710+Ashlirankin18@users.noreply.github.com>
2019-08-19 14:05:02 -0400

    Update FavoritesTableViewControllerDatasource.swift

--- MeetupClone/Controllers/DataSource/FavoritesTableViewControllerDatasource.swift
+++ MeetupClone/Controllers/DataSource/FavoritesTableViewControllerDatasource.swift
@@ -14,14 +14,14 @@ final class FavoritesTableViewControllerDatasource: NSObject, UITableViewDataSou
     // MARK: - UITableViewDataSource
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return PersistenceHelper.retrieveFavoriteEventsFromDocumentsDirectory().count
+        return PersistenceHelper.shared.retrieveFavoriteEventsFromDocumentsDirectory().count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "EventDisplayCell", for: indexPath) as? EventDisplayTableViewCell else {
             return UITableViewCell()
         }
-        let event = PersistenceHelper.retrieveFavoriteEventsFromDocumentsDirectory().reversed()[indexPath.row]
+        let event = PersistenceHelper.shared.retrieveFavoriteEventsFromDocumentsDirectory().reversed()[indexPath.row]
         
         cell.viewModel = EventDisplayTableViewCell.ViewModel(eventName: event.eventName, eventDescription: event.description ?? NSLocalizedString("This event does not have a description", comment: "Informs the user the event has no description currently."), eventLocation: event.eventCity, rsvpCount: event.rsvpCount)
         return cell
