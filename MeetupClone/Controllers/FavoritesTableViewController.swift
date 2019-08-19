95b616a715c83398aabb02a43bdc4598639a5d97
Ashli Rankin <ashlirankin18@gmail.com>
2019-08-19 12:49:47 -0400

    Added the didSelect delegate method to segue to the eventDetailedViewController

--- MeetupClone/Controllers/FavoritesTableViewController.swift
+++ MeetupClone/Controllers/FavoritesTableViewController.swift
@@ -17,7 +17,6 @@ final class FavoritesTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configureTableViewProperties()
-        title = NSLocalizedString("My Favorites", comment: "Indicates to the user the tab they are on")
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -29,4 +28,25 @@ final class FavoritesTableViewController: UITableViewController {
         tableView.register(UINib(nibName: "EventDisplayTableViewCell", bundle: Bundle.main), forCellReuseIdentifier: "EventDisplayCell")
         tableView.dataSource = favoritesTableViewControllerDataSource
     }
-}
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+     let event = persistenceHelper.shared.retrieveFavoriteEventsFromDocumentsDirectory()[indexPath.row]
+     let detailedController = EventDetailedTableViewController(style: .grouped)
+     detailedController.meetupEventModel = event
+     navigationController?.pushViewController(detailedController, animated: true)
+    }
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        guard let headerView = Bundle.main.loadNibNamed("GroupDisplayTableViewCell", owner: self, options: nil)?.first as? GroupDisplayTableViewCell else {
+            return UIView()
+        }
+        guard let url = URL(string: "https://thememylogin.com/app/uploads/edd/2019/03/favorites.png") else {
+            return UIView()
+        }
+        
+        headerView.viewModel = GroupDisplayTableViewCell.ViewModel(groupName: nil, groupImage: url, members: nil, nextEventName: nil)
+        return headerView
+    }
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 300
+    }
+} 
