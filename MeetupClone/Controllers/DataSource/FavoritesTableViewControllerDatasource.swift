340f86f8ce6b53fda8f6c8dd2f4e605fda8938d9
Ashli Rankin <42153710+Ashlirankin18@users.noreply.github.com>
2019-08-23 11:16:42 -0400

    Update FavoritesTableViewControllerDatasource.swift

--- MeetupClone/Controllers/DataSource/FavoritesTableViewControllerDatasource.swift
+++ MeetupClone/Controllers/DataSource/FavoritesTableViewControllerDatasource.swift
@@ -14,25 +14,25 @@ final class FavoritesTableViewControllerDataSource: NSObject, UITableViewDataSou
     // MARK: - UITableViewDataSource
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        let favorites = PersistenceHelper.shared.retrieveFavoriteEventsFromDocumentsDirectory()
+        let favorites = PersistenceHelper.shared.favoriteEvents
         if favorites.isEmpty {
             return 1
         } else {
-            return PersistenceHelper.shared.retrieveFavoriteEventsFromDocumentsDirectory().count
+            return favorites.count
         }
         
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-      let favorite = PersistenceHelper.shared.retrieveFavoriteEventsFromDocumentsDirectory()
-        if favorite.isEmpty {
-           let cell = tableView.dequeueEmptyStateCellAtIndexPath(cell: EmptyStateTableViewCell(), indexPath: indexPath, prompt: NSLocalizedString("You have not favorite events", comment: "Indicates to the user the have no favorite events"), image: UIImage.noFavoritesFound)
+        let favorites = PersistenceHelper.shared.favoriteEvents
+        if favorites.isEmpty {
+            let cell = tableView.dequeueEmptyStateCellAtIndexPath(cell: EmptyStateTableViewCell(), indexPath: indexPath, prompt: NSLocalizedString("You have not favorite events", comment: "Indicates to the user the have no favorite events"), image: UIImage.noFavoritesFound)
             return cell
         } else {
             guard let cell = tableView.dequeueReusableCell(withIdentifier: "EventDisplayCell", for: indexPath) as? EventDisplayTableViewCell else {
                 return UITableViewCell()
             }
-            let event =  PersistenceHelper.shared.retrieveFavoriteEventsFromDocumentsDirectory().reversed()[indexPath.row]
+            let event =  favorites[indexPath.row]
             do {
                 let description = try event.description?.asHTMLAttributedString()
                 cell.viewModel = EventDisplayTableViewCell.ViewModel(eventName: event.eventName, eventDescription: description?.string ?? NSLocalizedString("This event does not have a description", comment: "Informs the user the event has no description currently."), eventLocation: event.venue?.city, rsvpCount: event.yesRSVPCount)
