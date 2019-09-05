7f8e30d5396cfaeb19e42c286b26f1fd4892dfca
Ashli Rankin <ashlirankin18@gmail.com>
2019-09-05 16:16:05 -0400

    removed color clayRed

--- MeetupClone/Controllers/DataSource/UIColor+Extension.swift
+++ MeetupClone/Controllers/DataSource/UIColor+Extension.swift
@@ -9,8 +9,6 @@
 import UIKit
 
 extension UIColor {
-    /// Represents the clayRed color located in the assets catalog
-    static let clayRed = UIColor(named: "ClayRed", in: Bundle.main, compatibleWith: .none)
     
     /// Represents the Red color located in the assets catalog
     static let red = UIColor(named: "Red", in: Bundle.main, compatibleWith: .none)
